f72074a23816d05391c886d18b9546dd01bddefd
jashikakaur <144861801+jashikakaur@users.noreply.github.com>
2025-04-15 13:36:39 -0400

    Update extractImageWithYTDLP.py
    now RATIO INCLUDED IN OUTPUT!!!!!!!!!

--- extractImageWithYTDLP.py
+++ extractImageWithYTDLP.py
@@ -1,97 +1,3 @@
-
-# # pylint: disable=no-member
-# import yt_dlp
-# import subprocess
-# import csv
-# import cv2
-# import os
-# import time
-
-# # Gets the metadata with the ytdlp extract info stuff and then it gets the duration field which gives the duration
-# def get_video_duration(url):
-#     ytdlp_options = {'quiet': True, 'no_warnings': True,}
-#     with yt_dlp.YoutubeDL(ytdlp_options) as yt:
-#         info = yt.extract_info(url, download=False)
-#         return info.get("duration", 0)
-
-# def get_frame(url, start_time, output_file):
-#     # download a small segment .05 seconds starting at start_time.
-#     segment_duration = .05
-#     end_time = start_time + segment_duration
-#     segment_file = "temp_segment.mp4"
-    
-#     # Download only the required section.
-#     # Note: start_time and end_time are in seconds.
-#     command = (
-#         f'yt-dlp --download-sections "*{start_time}-{end_time}" -f best '
-#         f'-o "{segment_file}" "{url}"'
-#     )
-#     subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-
-    
-#     # Now extract a single frame from the downloaded segment.
-#     # Because the segment’s timeline starts at 0, we seek from 0 (or a small offset if desired).
-#     command = (
-#         f'ffmpeg -hide_banner -loglevel error -ss 0 -i "{segment_file}" '
-#         f'-vframes 1 -q:v 2 -pix_fmt yuvj420p "{output_file}"'
-#     )
-#     subprocess.call(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    
-#     # Remove the temporary segment file.
-#     os.remove(segment_file)
-    
-# # this is gonna have arrays of each videos first, middle, and last frames    
-# video_frames = []
-
-# with open('videos.csv', mode='r') as video:
-    
-#     the_row = next(csv.reader(video))
-#     count = 0
-    
-#     # im going through the csv file
-#     for video_id in the_row:
-#         count+=1
-        
-#         url = "https://www.youtube.com/watch?v=" + video_id
-#         print(f'---------------------------------------------\n🎬 Starting video #{count}\n---------------------------------------------')
-        
-#         duration = get_video_duration(url)
-        
-#         first_second = duration * .01
-#         middle_second = duration *.5
-#         last_second = duration * .9
-        
-#         # temporarily making images of the files
-#         first_file = f"{video_id}_first.jpg"
-#         middle_file = f"{video_id}_middle.jpg"
-#         last_file = f"{video_id}_last.jpg"
-        
-#         # get the frames from the beginning middle and end.
-#         get_frame(url, first_second, first_file)
-#         print(f'🥚 Got first frame for video {count}: {url}')
-#         get_frame(url, middle_second, middle_file)
-#         print(f'🐣 Got middle frame for video {count}: {url}')
-#         get_frame(url, last_second, last_file)
-#         print(f'🐥 Got last frame for video {count}: {url}')
-    
-#         first_img = cv2.imread(first_file)
-#         middle_img = cv2.imread(middle_file)
-#         last_img = cv2.imread(last_file)
-        
-#         # put all the frames in an array with the url so we know which videos have the frames
-#         video_frames.append([url ,first_img, middle_img, last_img])
-        
-#         # get rid of the temporary files.
-#         os.remove(first_file)
-#         os.remove(middle_file)
-#         os.remove(last_file)
-        
-#         print(f'🪺 Sleep #{count} videos finished')
-#         time.sleep(2)
-        
-# print("success")
-        
-        
 # pylint: disable=no-member
 import yt_dlp
 import subprocess
@@ -135,8 +41,14 @@ def get_video_dimensions(url):
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
    
     cap.release()
-    print(width, height)
-    return width, height
+
+   ratio = "Square"
+    if height > width
+        ratio = "vertical"
+    elif width > height:
+        ratio = "horizontal"
+    
+    return width, height, ratio
 
 
 def find_crop(url, video_id):
@@ -209,9 +121,9 @@ with open('videos.csv', mode='r') as video:
       # save output of find_crop 
         #find_crop(url, video_id)
 
-        width, height = get_video_dimensions(url)
+        width, height, ratio = get_video_dimensions(url)
         if (width and height):
-            save_dimensions_to_csv(video_id, width, height)
+            save_dimensions_to_csv(video_id, width, height, ratio)
 
         
         print(f'🪺 Sleep #{count} videos finished')
